a1ba2c704c1096f5eb7b7f70664d22f689714aae
jtsiddons <jtsiddons@gmail.com>
2024-05-31 11:28:50 +0100

    mod: window separator and inactive lualine to light

--- lua/lualine/themes/kanagawa.lua
+++ lua/lualine/themes/kanagawa.lua
@@ -3,41 +3,41 @@ local theme = require("kanagawa.colors").setup().theme
 local kanagawa = {}
 
 kanagawa.normal = {
-  a = { bg = theme.syn.fun, fg = theme.ui.bg_m3 },
-  b = { bg = theme.diff.change, fg = theme.syn.fun },
-  c = { bg = theme.ui.bg_p1, fg = theme.ui.fg },
+	a = { bg = theme.syn.fun, fg = theme.ui.bg_m3 },
+	b = { bg = theme.diff.change, fg = theme.syn.fun },
+	c = { bg = theme.ui.bg_p1, fg = theme.ui.fg },
 }
 
 kanagawa.insert = {
-  a = { bg = theme.diag.ok, fg = theme.ui.bg },
-  b = { bg = theme.ui.bg, fg = theme.diag.ok },
+	a = { bg = theme.diag.ok, fg = theme.ui.bg },
+	b = { bg = theme.ui.bg, fg = theme.diag.ok },
 }
 
 kanagawa.command = {
-  a = { bg = theme.syn.operator, fg = theme.ui.bg },
-  b = { bg = theme.ui.bg, fg = theme.syn.operator },
+	a = { bg = theme.syn.operator, fg = theme.ui.bg },
+	b = { bg = theme.ui.bg, fg = theme.syn.operator },
 }
 
 kanagawa.visual = {
-  a = { bg = theme.syn.keyword, fg = theme.ui.bg },
-  b = { bg = theme.ui.bg, fg = theme.syn.keyword },
+	a = { bg = theme.syn.keyword, fg = theme.ui.bg },
+	b = { bg = theme.ui.bg, fg = theme.syn.keyword },
 }
 
 kanagawa.replace = {
-  a = { bg = theme.syn.constant, fg = theme.ui.bg },
-  b = { bg = theme.ui.bg, fg = theme.syn.constant },
+	a = { bg = theme.syn.constant, fg = theme.ui.bg },
+	b = { bg = theme.ui.bg, fg = theme.syn.constant },
 }
 
 kanagawa.inactive = {
-  a = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
-  b = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim, gui = "bold" },
-  c = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+	a = { bg = theme.ui.fg, fg = theme.ui.bg_dim },
+	b = { bg = theme.ui.fg, fg = theme.ui.bg_dim, gui = "bold" },
+	c = { bg = theme.ui.fg, fg = theme.ui.bg_dim },
 }
 
 if vim.g.kanagawa_lualine_bold then
-  for _, mode in pairs(kanagawa) do
-    mode.a.gui = "bold"
-  end
+	for _, mode in pairs(kanagawa) do
+		mode.a.gui = "bold"
+	end
 end
 
 return kanagawa
